f2a75641ae5d3e0cced7c09f2c84c5114e948b7e
Naureen Imran <naureen.imran@in4group.co.uk>
2025-01-13 13:53:44 +0000

    create three turtles

--- turtle_example.py
+++ turtle_example.py
@@ -1,3 +1,31 @@
-print("hello")
-print("hello world!")
-print("hi ")
+import turtle
+from turtle import *
+from random import randint
+
+john = Turtle()
+bob = Turtle
+sally = Turtle()
+
+john.color('red')
+bob.color('green')
+sally.color('blue')
+
+john.penup()
+john.goto(-160, 100)
+john.pendown()
+
+bob.penup()
+bob.goto(-160, 70)
+bob.pendown()
+
+sally.penup()
+sally.goto(-160, 40)
+sally.pendown()
+
+for movement in range(100):
+    john.forward(randint(1,5))
+    bob.forward(randint(1,5))
+    sally.forward(randint(1,5))
+
+
+input("Press Enter to  close")
